e890c8b597c46fe7ea0b0ca9b6728f9c7b687191
Saurabh-827 <saurabhrabta@gmail.com>
2025-05-28 16:08:13 +0530

    Add database migration on application startup

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
